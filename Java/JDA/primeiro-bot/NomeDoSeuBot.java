2f991263142aa466f3f10e165c7ab9875d3b2fee
kaikeDev <caheasta@gmail.com>
2017-10-26 20:17:54 -0200

    Adicionado a main class

---
+++ Java/JDA/primeiro-bot/NomeDoSeuBot.java
@@ -0,0 +1,17 @@
+
+//Esta implementando o EventListener, Essa sera a classe "Principal do seu bot"
+public class NomeDoSeuBot  {
+
+ //Isso registra como sua classe principall
+ public static void main(String[] args)
+            throws LoginException, RateLimitedException, InterruptedException
+    {
+        //Aqui e para ligar seu bot
+                                //E Importante Colocar Como AccountType.BOT para que a api possa saber que não e um selfbot
+        JDA jda = new JDABuilder(AccountType.BOT)
+            .setToken("token")
+            .buildBlocking();
+    }
+    
+
+}
