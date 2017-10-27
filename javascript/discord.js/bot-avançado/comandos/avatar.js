98016210f26efae752872184d297ff8673a6f5c2
BannerGames <davidandrade339@gmail.com>
2017-10-27 18:10:00 +0100

    Update avatar.js

--- javascript/discord.js/bot-avançado/comandos/avatar.js
+++ javascript/discord.js/bot-avançado/comandos/avatar.js
@@ -1,12 +1,13 @@
 exports.run = (client, message, args) => {
+ const Discord = require("discord.js")
  let usuario = message.mentions.users.first() || message.author; // Pega a menção ou, se não houver menção, de quem executou o comando.
-        if (user.id == message.author.id) { // Se não houver menção manda o avatar do autor.
+        if (usuario.id == message.author.id) { // Se não houver menção manda o avatar do autor.
         const embed = new Discord.RichEmbed()
              .setTitle('O seu avatar:')
              .setImage(message.author.avatarURL); // Avatar do autor
         message.channel.send({embed: embed}) // Envia a embed. 
         
-        } else if (user.id !== message.author.id) { // Se houver menção manda o avatar do usuário mencionado.
+        } else if (usuario.id !== message.author.id) { // Se houver menção manda o avatar do usuário mencionado.
             const embed = new Discord.RichEmbed()
             .setTitle(`Avatar de ${user.username}:`)
             .setImage(user.avatarURL); // Avatar do usuário mencionado
