45298b8868c4ab82d9f6f9e467ee551db76db3dc
davipatury <davi.patury@yahoo.com.br>
2017-10-27 19:31:25 -0200

    Java branch

--- javascript/discord.js/bot-avançado/comandos/ping.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message, args) => {
-    const ping = Math.round(client.ping); // Pega no ping do bot e arredonda, de modo a ser um ping mais compreensível.
-    message.channel.send(`Pong! ${ping} ms`);
-}
