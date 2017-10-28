e62097ab8fb4084366b4f8530dedc0c5ef89bf86
kaikeDev <caheasta@gmail.com>
2017-10-28 19:54:03 -0200

    Adicionado um simples bufferedimage

--- jda/primeiro-bot/DiscordListener.java
+++ jda/primeiro-bot/DiscordListener.java
@@ -21,6 +21,29 @@
           //Ele vai me responder com Pong!
               //É Importante deixar .queue();
           event.getTextChannel().sendMessage("Pong!").queue();
+          
+          
+        //Vamos adicionar uma imagem
+         //Para isso, vamos usar um bufferedimage
+         message.startsWith("_simpleimage") {
+            //Vamos criar uma bufferedimage com o tamanho 400 x 300
+            BufferedImage base = BufferedImage(400, 300, BufferedImage.TYPE_INT_ARGB);
+
+            //Para facilitar o code
+            Graphics graphics = base.graphics;
+            //Vamos colocar a cor do texto de branco
+             graphics.setColor = Color(255,255,255);
+             //Agora vamos escrever alguma coisa na nossa bufferedimage
+              graphics.drawString("Hello World ", 15, 70);
+                //Agora vamos configurar a saida da imagem
+                 ByteArrayOutputStream os = ByteArrayOutputStream();
+                 ImageIO.write(base, "png", os);
+                 ByteArrayInputStream inputStream = ByteArrayInputStream(os.toByteArray());
+                 //Agora por ultimo vamos enviar a mensagem para o user
+               event.textChannel.sendFile(inputStream, "hello.png", MessageBuilder().append("Texto Qualquer").build()).queue();
+
+
+        }
         }
      }
  }
