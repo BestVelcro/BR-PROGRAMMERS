7132538e023a7b5d4abcbfe13cd5352d02ba3ad4
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-27 23:45:32 -0200

    eris

--- eris/bot simples/bot.js
+++ eris/bot simples/bot.js
@@ -1,18 +1,37 @@
-var Eris = require('eris');
+/*
+ * Simples require da lib Eris.
+ */
+const Eris = require('eris');
 
-var bot = new Eris("token_do_seu_bot");
-// Troque o token_do_seu_bot pelo token da conta do bot
+/*
+ * Instanciando novo client, simplesmente troque o token_do_seu_bot pelo token do seu bot (dur).
+ */
+const bot = new Eris('token_do_seu_bot');
 
-bot.on("ready", () =>{// Evento: quando o bot estiver pronto
-	console.log("I'm Ready!");// enviará no log "I'm Ready!"
+/*
+ * Listener pro evento "ready" que é emitido quando o bot se conectar com a api do discord.
+ * Neste caso o callback fará um simples console.log para avisar que o bot conectou com sucesso.
+ */
+bot.on('ready', () => {
+  console.log('Conectado =)');
 });
 
-bot.on("messageCreate", (msg) => {// Evento: quando uma mensagem for criada
-
-if(msg.content.indexOf("ping") == 0){//Se o começo da mensagem for "ping"
-bot.createMessage(msg.channel.id, "pong!");//envie a mensagem "pong!"
-}
-
+/*
+ * Este listener vai escutar o evento "messageCreate" que é emitido
+ * quando uma mensagem é enviada para um servidor que o seu bot está.
+ * Se a mensagem começar com "ping" o bot vai responder com um "pong!".
+ */
+bot.on('messageCreate', (msg) => {
+  if (msg.content.indexOf('ping') == 0) {
+    /*
+     * createMessage é uma função que envia uma mensagem nova, recebe como parametros
+     * o ID do canal que a mensagem deve ser enviada e o conteúdo da mensagem.
+     */
+    bot.createMessage(msg.channel.id, 'pong!');
+  }
 });
 
-bot.connect();// conecta o bot para o Discord
+/*
+ * Starta o bot.
+ */
+bot.connect();
