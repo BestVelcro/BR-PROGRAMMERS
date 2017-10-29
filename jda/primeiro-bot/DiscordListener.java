2c6beb03e67c255d2500aecbb3b03efd3b484db3
kaikeDev <caheasta@gmail.com>
2017-10-29 17:06:27 -0200

    Ajdeitando meu codigo errado

--- jda/primeiro-bot/DiscordListener.java
+++ jda/primeiro-bot/DiscordListener.java
@@ -17,30 +17,34 @@
        String message = event.getMessage().getContent();
         
         //Se a mensagem começar com _ping...
-        message.startsWith("_ping")) { 
+      if(message.startsWith("_simpleimage") {
           //Ele vai me responder com Pong!
               //É Importante deixar .queue();
           event.getTextChannel().sendMessage("Pong!").queue();
+          }
           
           
         //Vamos adicionar uma imagem
          //Para isso, vamos usar um bufferedimage
          message.startsWith("_simpleimage") {
             //Vamos criar uma bufferedimage com o tamanho 400 x 300
-            BufferedImage base = BufferedImage(400, 300, BufferedImage.TYPE_INT_ARGB);
+             BufferedImage base = new BufferedImage(400, 300, BufferedImage.TYPE_INT_ARGB);
 
             //Para facilitar o code
-            Graphics graphics = base.graphics;
+            Graphics graphics = base.getGraphics();
             //Vamos colocar a cor do texto de branco
-             graphics.setColor = Color(255,255,255);
+             graphics.setColor(Color.WHITE);
              //Agora vamos escrever alguma coisa na nossa bufferedimage
               graphics.drawString("Hello World ", 15, 70);
                 //Agora vamos configurar a saida da imagem
-                 ByteArrayOutputStream os = ByteArrayOutputStream();
+                 ByteArrayOutputStream os = new ByteArrayOutputStream();
                  ImageIO.write(base, "png", os);
-                 ByteArrayInputStream inputStream = ByteArrayInputStream(os.toByteArray());
+                  ByteArrayInputStream inputStream = new ByteArrayInputStream(os.toByteArray());
                  //Agora por ultimo vamos enviar a mensagem para o user
-               event.textChannel.sendFile(inputStream, "hello.png", MessageBuilder().append("Texto Qualquer").build()).queue();
+               
+
+event.getTextChannel().sendFile(inputStream, "hello.png", new MessageBuilder().append("Texto Qualquer").build()).queue();
+
 
 
         }
